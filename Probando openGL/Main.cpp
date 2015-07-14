297dcf7fd84b4093ac50b180b548a488cdbf6d33
Luis Almaguer <imaximuz@gmail.com>
2015-07-13 19:46:52 -0500

    Todo listo para empezar

---
+++ Probando openGL/Main.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <gl\glfw3.h>
+#include <gl\glut.h>
+
+#pragma comment(lib,"glfw3.lib")
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+
+int main(void){
+
+
+
+	return 0;
+}
