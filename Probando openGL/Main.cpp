256a65ad6c2160b925ff8b9232fe3a3eb47efdcd
Luis Almaguer <imaximuz@gmail.com>
2015-07-13 20:28:03 -0500

    Configurando la ventana

--- Probando openGL/Main.cpp
+++ Probando openGL/Main.cpp
@@ -2,7 +2,35 @@
 
 int main(void){
 
+	GLFWwindow* window;
+
+	if ( !glfwInit() )
+		return -1;
 
 
+	glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
+	glfwWindowHint( GLFW_DOUBLEBUFFER, GL_TRUE );
+	
+	window = glfwCreateWindow( 800, 600, "OpenGL Game", NULL, NULL );
+	
+	if ( !window )
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent( window );
+
+	while ( !glfwWindowShouldClose( window ) )
+	{
+
+		glClear( GL_COLOR_BUFFER_BIT );
+
+
+		glfwSwapBuffers( window );
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
