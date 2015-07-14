51bd6666cf772623aefb5452edc66337ffee148d
Luis Almaguer <imaximuz@gmail.com>
2015-07-13 19:52:18 -0500

    Agregado el Precompiled.h-cpp

--- Probando openGL/Main.cpp
+++ Probando openGL/Main.cpp
@@ -1,12 +1,4 @@
-#include <Windows.h>
-#include <gl\GL.h>
-#include <gl\GLU.h>
-#include <gl\glfw3.h>
-#include <gl\glut.h>
-
-#pragma comment(lib,"glfw3.lib")
-#pragma comment(lib,"opengl32.lib")
-#pragma comment(lib,"glu32.lib")
+#include "Precompiled.h"
 
 int main(void){
 
---
+++ Probando openGL/Precompiled.h
@@ -0,0 +1,39 @@
+#ifndef _PRECOMPILED_H_
+#define _PRECOMPILED_H_
+
+//If the compiler defines one of these items we can assume that it is windows running
+#if defined(__WIN32__) || defined(_WIN32) || defined(WIN32) || defined(__WINDOWS__) || defined(__TOS_WIN__)
+//so we will standardize on WIN32 for windows specific code
+#ifndef WIN32
+#define WIN32
+#endif
+#endif
+
+#ifdef WIN32
+#include <Windows.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <gl\glfw3.h>
+#include <gl\glut.h>
+
+#pragma comment(lib,"glfw3.lib")
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+
+#endif
+
+#undef min
+#undef max
+
+#include <string>
+#include <cstring> // strlen, memcpy, etc.
+#include <cstdlib> // exit
+#include <cfloat>  // FLT_MAX
+#include <vector>
+#include <math.h>
+#include <gl\GLU.h>
+#include <gl\GL.h>
+#include <gl\glut.h>
+
+
+#endif // PRECOMPILED_H
